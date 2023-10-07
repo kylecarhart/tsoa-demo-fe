dda2cac45be7860a962b38801f92829992aca91c
Kyle Carhart <77036988+kylecarhart@users.noreply.github.com>
2023-10-06 22:37:45 -0400

    added orders page

---
+++ app/orders/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { Order, OrdersApi } from "../../@generated/src";
+import { handleApiErrorResponse } from "../../utils/handleApiErrorResponse";
+import toast from "react-hot-toast";
+import styles from "./page.module.css";
+
+export default function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    async function getItems() {
+      try {
+        const orders = await new OrdersApi().getAllOrders();
+        setOrders(orders);
+      } catch (e) {
+        const error = await handleApiErrorResponse(e);
+        toast.error(error.message);
+      }
+    }
+
+    getItems();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      {orders.map((order) => (
+        <div key={order.id} className={styles.order}>
+          <div>{order.id}</div>
+          <div>
+            {order.items.map((orderItem) => (
+              <div key={orderItem.item.id}>
+                <div>{orderItem.item.name}</div>
+                <div>{orderItem.item.price}</div>
+                <div>{orderItem.quantity}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
